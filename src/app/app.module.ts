6833375da2b9dc8caa0027275374b688719fec2f
diouf <thirtsoft@gmail.com>
2019-12-03 03:42:45 +0000

    Mise en place du Front-End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MailModalComponent } from './modal/mail-modal/mail-modal.component';
+import { MessageModalComponent } from './modal/message-modal/message-modal.component';
+import { BackToMenuComponent } from './menu/back-to-menu/back-to-menu.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { BookComponent } from './book/book.component';
+import { CustomerComponent } from './customer/customer.component';
+import { LoanComponent } from './loan/loan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MailModalComponent,
+    MessageModalComponent,
+    BackToMenuComponent,
+    MenuComponent,
+    BookComponent,
+    CustomerComponent,
+    LoanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modal/mail-modal/mail-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-modal',
+  templateUrl: './mail-modal.component.html',
+  styleUrls: ['./mail-modal.component.scss']
+})
+export class MailModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
